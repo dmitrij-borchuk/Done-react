47fec2394798f59daeac25319084c70ab4b6f0c0
dborchuk <frunk.lern@gmail.com>
2020-06-06 19:10:05 +0300

    Added list tests

--- cypress/integration/tests/app.spec.js
+++ cypress/integration/tests/app.spec.js
@@ -17,4 +17,14 @@ context('Window', () => {
     })
     cy.getById('list-item').its('length').should('eq', 2)
   })
+
+  it('should render crossed item', () => {
+    cy.server()
+    cy.route({
+      method: 'GET',
+      url: `${Cypress.env('serverUrl')}/data/items`,
+      response: [{ title: 'ToDo 1', done: true, objectId: 'id1' }],
+    })
+    cy.getById('list-item').first().should('have.class', 'line-through')
+  })
 })
